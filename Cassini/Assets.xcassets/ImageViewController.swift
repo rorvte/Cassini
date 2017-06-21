7f40e03a3d4b94ad930aa139529b283e2d61b9ec
yolanda <rorvte@gmail.com>
2017-06-21 17:35:45 +0800

    initial commit

---
+++ Cassini/Assets.xcassets/ImageViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ImageViewController.swift
+//  Cassini
+//
+//  Created by yolanda yuan on 20/6/2017.
+//  Copyright © 2017 yolanda yuan. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+
+    var imageURL: URL? {
+        didSet {
+            image = nil
+            if view.window != nil {
+                fetchImage()
+            }
+        }
+    }
+    
+    private func fetchImage() {
+        if let url = imageURL {
+            let urlContents = try? Data(contentsOf: url)
+            if let imageData = urlContents {
+                image = UIImage(data: imageData)
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageURL = URL(string: "https://pic2.zhimg.com/v2-adc9242cc79826ca4bfff2866a58e649_r.png")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if image == nil {
+            fetchImage()
+        }
+    }
+    
+    @IBOutlet weak var scrollView: UIScrollView! {
+        didSet {
+            scrollView.delegate = self
+            scrollView.minimumZoomScale = 0.03
+            scrollView.maximumZoomScale = 3
+            scrollView.contentSize = imageView.frame.size
+            scrollView.addSubview(imageView)
+        }
+    }
+    
+    fileprivate var imageView = UIImageView()
+    
+    private var image: UIImage? {
+        get {
+            return imageView.image
+        }
+        set {
+            imageView.image = newValue
+            imageView.sizeToFit()
+            scrollView?.contentSize = imageView.frame.size
+        }
+    }
+}
+
+extension ImageViewController : UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+}
---
+++ Cassini/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cassini
+//
+//  Created by yolanda yuan on 20/6/2017.
+//  Copyright © 2017 yolanda yuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
